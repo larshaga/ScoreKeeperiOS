f2457d06b5cb0793ec9cb517e8f42286f7b61c38
Lars Kristian Haga <larskhaga@gmail.com>
2020-04-14 15:10:39 +0200

    Initial Commit

---
+++ ScoreKeeper WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  ScoreKeeper WatchKit Extension
+//
+//  Created by Lars Kristian Haga on 14/04/2020.
+//  Copyright © 2020 Lars Kristian Haga. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ ScoreKeeper WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  ScoreKeeper WatchKit Extension
+//
+//  Created by Lars Kristian Haga on 14/04/2020.
+//  Copyright © 2020 Lars Kristian Haga. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
