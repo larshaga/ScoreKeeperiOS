3959128db788f965136c2f1cae3283be27f1fb3d
Lars Kristian Haga <larskhaga@gmail.com>
2020-04-14 21:24:34 +0200

    Added buttons that count the number of goals, but still need more works

--- ScoreKeeper WatchKit Extension/ContentView.swift
+++ ScoreKeeper WatchKit Extension/ContentView.swift
@@ -1,16 +1,52 @@
-//
-//  ContentView.swift
-//  ScoreKeeper WatchKit Extension
-//
-//  Created by Lars Kristian Haga on 14/04/2020.
-//  Copyright © 2020 Lars Kristian Haga. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var homeNumberOfGoals = 0;
+    @State private var awayNumberOfGoals = 0;
+    @GestureState var isDetectingLongPress = false
+    @State var completedLongPress = false
+    
+    var longPress: some Gesture {
+        LongPressGesture(minimumDuration: 1)
+        .updating($isDetectingLongPress) { currentstate, gestureState,
+                transaction in
+        }
+            .onEnded({ finished in
+                self.completedLongPress = finished
+                self.homeNumberOfGoals = 1337;
+                self.awayNumberOfGoals = 69696969;
+            })}
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: {
+//                self.showDetails.toggle()
+                self.homeNumberOfGoals+=1;
+            }){
+                Text(String(self.homeNumberOfGoals))
+            }
+            .gesture(longPress)
+
+            
+            Button(action: {
+                self.awayNumberOfGoals+=1;
+            }
+                )
+            {
+                Text(String(self.awayNumberOfGoals))
+            }
+            .gesture(longPress)
+            
+            
+//            if showDetails {
+//                Text("MÅÅL")
+//            }
+            Circle()
+            .fill(self.isDetectingLongPress ?
+                Color.red :
+                    (self.completedLongPress ? Color.green : Color.blue))
+            .gesture(longPress)
+        }
     }
 }
 
